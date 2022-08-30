40823dafc03bc269d147786252c466b13575e6d9
Alton Bonilla <Alton77@csu.fullerton.edu>
2022-08-29 21:18:24 -0700

    I dont understand one bit

--- part-2/quadratic_formula.cc
+++ part-2/quadratic_formula.cc
@@ -1,5 +1,11 @@
-
-// TODO: don't forget to put your header at the top of every file. And don't forget to remove this comment!
+// Christian Bonilla
+// CPSC 120-01
+// 2022-8-29
+// Alton77@csu.fullerton.edu
+// @alton7759
+//
+// Lab 2
+// Partners: @peteranteater
 
 #include <cmath>
 #include <iostream>
@@ -15,23 +21,24 @@ int main(int argc, char const *argv[]) {
   // For integer variables assigning them 0 or other integer value is a good
   // practice. For doubles and floats using NAN which means 'not a number' is a
   // good value to use.
-  double a = NAN;
-  double B = NAN;
-  double c = NAN;
-  double DISCRIMINANT = NAN;
+  double aaa = NAN;
+  double bbb = NAN;
+  double ccc = NAN;
+  double discriminant = NAN;
   // In 4x^2, a is 4
-  a = 4.0;
+  aaa = 4.0;
   // In + 7x, b is 7
-  B = 7.0;
+  bbb = 7.0;
   // In - 13, c is -13
-  c = -13.0;
-  DISCRIMINANT = (B * B) - (4 * a * c);
-  if (DISCRIMINANT >= 0 and a != 0.0) {
-  double FirstSolution = (-B + sqrt(DISCRIMINANT)) / (2 * a);
-  double second_solution = (-B - sqrt(DISCRIMINANT)) / (2 * a);
+  ccc = -13.0;
+  discriminant = (bbb * bbb) - (4 * aaa * ccc);
+  if (discriminant >= 0 and aaa != 0.0) {
+  double firstsolution = (-bbb + sqrt(discriminant)) / (2 * aaa);
+  double second_solution = (-bbb - sqrt(discriminant)) / (2 * aaa);
   cout << "There are two solutions for 4x^2 + 7x - 13 = 0.\n";
-  cout << "The first is " << FirstSolution << " and the second is " << second_solution << ".\n";
-  } else
+  cout << "The first is " << firstsolution << " and the second is " << second_solution << ".\n";
+  } else {
   cout << "There are no Real roots for 4x^2 + 7x - 13 = 0\n";
-  return 0;
+  return 0;}
+  
 }
