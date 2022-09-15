af3bc34855aa195fd49022d7ddb7550cb0909aed
Christian Bonilla <Alton77@csu.fullerton.edu>
2022-09-15 00:14:32 -0400

    getting it done?

--- part-3/hello.cc
+++ part-3/hello.cc
@@ -1,3 +1,12 @@
+//Christian Alton Bonilla
+//CPSC 120-01
+//2022-8-29
+//Alton@csu.fullerton.edu
+//@Alton7759
+//
+//Lab 2
+//Partners:
+//this says hello world
 #include <iostream>
 
 using namespace std;
